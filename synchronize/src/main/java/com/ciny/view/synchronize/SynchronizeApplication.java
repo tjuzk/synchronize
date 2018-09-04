a3866e0adfae33a7c4dee6ac58f559de3ac54100
tjuzk <1098473085@qq.com>
2018-09-04 22:30:52 +0800

    Project initalize

---
+++ synchronize/src/main/java/com/ciny/view/synchronize/SynchronizeApplication.java
@@ -0,0 +1,12 @@
+package com.ciny.view.synchronize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizeApplication.class, args);
+	}
+}
